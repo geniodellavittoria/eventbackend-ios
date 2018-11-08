0a8094eecb20fcf862064487f587bf9d9b887be4
Pascal Keusch <pascal.keusch@gmail.com>
2018-11-08 21:50:47 +0100

    import jdl file

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,20 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { EventAppRegistrationCategoryModule } from './registration-category/registration-category.module';
+import { EventAppUserEventRegistrationModule } from './user-event-registration/user-event-registration.module';
+import { EventAppCategoryModule } from './category/category.module';
+import { EventAppEventModule } from './event/event.module';
+import { EventAppImpressionModule } from './impression/impression.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        EventAppRegistrationCategoryModule,
+        EventAppUserEventRegistrationModule,
+        EventAppCategoryModule,
+        EventAppEventModule,
+        EventAppImpressionModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
